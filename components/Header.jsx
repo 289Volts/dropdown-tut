553d5cbaf3b023a126e97ac63b8096618a66c7c6
289Volts <289volts@gmail.com>
2023-01-07 22:20:34 +0100

    [CHORE] Desktop nav hidden for mobile view

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,60 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link"
-import Logo from "../public/assets/images/logo.svg"
+import Link from "next/link";
+import Logo from "../public/assets/images/logo.svg";
+import Open from "../public/assets/icons/icon-menu.svg";
+import Close from "../public/assets/icons/icon-close-menu.svg";
 
 const Header = ({ fontClass }) => {
-    return <header className={`${fontClass}`}>
-        <div className="">
-            <Link href="/">
-                <Image src={Logo} alt="Logo" />
-            </Link>
-            
-            <nav className="desktop-nav">
-            
-            </nav>
-        </div>
+	return (
+		<header className={`${fontClass}`}>
+			<div className="desktop-nav">
+				<div className="header-r__side">
+					<Link href="/">
+						<Image src={Logo} alt="Logo" />
+					</Link>
+					<nav className="desktop-nav-items"></nav>
+				</div>
+				<div className="ctaBtn-container">
+					<ul className="">
+						<li className="">
+							<Link href="">Login</Link>
+						</li>
+						<li className="">
+							<Link href="">Register</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
 
-        <div className="ctaBtn-container">
-            <ul className="">
-                <li className=""><Link href="">Login</Link></li>
-                <li className=""><Link href="">Register</Link></li>
-            </ul>
-        </div>
-        
-    </header>;
+			<div className="mobile-nav">
+				<Link href="/">
+					<Image src={Logo} alt="Logo" />
+				</Link>
+				<Link href="/">
+					<Image src={Open} alt="Logo" />
+				</Link>
+
+				<nav className="mobile-nav-items">
+					<div className="close-btn">
+						<Link href="/">
+							<Image src={Close} alt="Logo" />
+						</Link>
+					</div>
+					<div className="ctaBtn-container">
+						<ul className="">
+							<li className="">
+								<Link href="">Login</Link>
+							</li>
+							<li className="">
+								<Link href="">Register</Link>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
